168280ee8d0a0f3684ae3278b9594c4e4d127d45
Roberto <montanes.rarob24@triana.salesianos.edu>
2026-01-16 09:06:49 +0100

    primera subida

---
+++ src/main/java/com/salesianostriana/dam/Eventify/Lite/eventifylite/EventifyLiteApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.Eventify.Lite.eventifylite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/Eventify/Lite/eventifylite/EventifyLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.Eventify.Lite.eventifylite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
